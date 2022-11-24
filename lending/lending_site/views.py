7f70ee516c48470f0da4ccd3fc7d08e3985768b6
paigni <ikontai4@gmail.com>
2022-11-24 20:23:07 +0600

    Добавлен новый класс

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404
+)
+from django.contrib import messages
+from django.views.generic import (
+    ListView,
+)
+from lending_site.models import (
+    Company,
+    Vacancy,
+    Applicants,
+)
+from lending_site.forms import ApplicantsForm
 
-# Create your views here.
+
+class IndexListView(ListView):
+    model1 = Company
+    model2 = Vacancy
+    form = ApplicantsForm
+    template_name = 'lending_site/index.html'
+    context = {}
+
+    def get(self, request):
+        self.context = {
+            'company': self.model1.objects.all(),
+            'vacancy': self.model2.objects.filter(activated=1)
+        }
+        return render(request, self.template_name, self.context, self.form)
