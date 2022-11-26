484665b117c627603d35602591f6ac451858154e
paigni <ikontai4@gmail.com>
2022-11-26 20:12:06 +0600

    тесты,в разработке

--- lending/lending_site/tests.py
+++ lending/lending_site/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from lending_site.service import (
+    get_queryset_company,
+    get_queryset_vacancy,
+    )
 
-# Create your tests here.
+
+class ServiceTestCase():
+    def test_gqc(self):
+        print(get_queryset_company)
+
+    def test_gqv(self):
+        print(get_queryset_vacancy.name)
