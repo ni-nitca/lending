15e64851098c85e3c33e13a16d23ade5fc9e5567
paigni <ikontai4@gmail.com>
2022-11-26 20:09:12 +0600

    Добавлено имя сайта

--- lending/lending_site/apps.py
+++ lending/lending_site/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LendingSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lending_site'
+    verbose_name = 'HR лендинг'
