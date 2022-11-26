016091dc21502dd9c575634117268e742b338dcc
paigni <ikontai4@gmail.com>
2022-11-26 20:11:47 +0600

    Правки,добавлена связь вакаансий с сосискателями

--- lending/lending_site/models.py
+++ lending/lending_site/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from solo.models import SingletonModel
 
+
 class Company(SingletonModel):
     name = models.CharField(
         verbose_name="Название",
@@ -16,19 +17,19 @@ class Company(SingletonModel):
     )
 
     def __str__(self):
-        return self.description
+        return self.name
 
-    class Meta():
+    class Meta:
         db_table = 'company'
         verbose_name = 'Компания'
         verbose_name_plural = 'Компании'
+
 
 class Contacts(models.Model):
     contact = models.ForeignKey(
         Company,
         verbose_name="Контакты",
         on_delete=models.CASCADE,
-        null=True
     )
     phone = models.CharField(
         verbose_name="Номер компании",
@@ -42,7 +43,8 @@ class Contacts(models.Model):
         verbose_name="Наш адрес",
         max_length=100,
     )
-    class Meta():
+
+    class Meta:
         db_table = 'contacts'
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
@@ -59,7 +61,7 @@ class Vacancy(models.Model):
     )
     salary = models.CharField(
         verbose_name="Заработная плата",
-        max_length=10,
+        max_length=64
     )
     create_date = models.DateTimeField(
         verbose_name='Дата создания',
@@ -69,17 +71,24 @@ class Vacancy(models.Model):
         verbose_name='Статус активности',
         default=True,
     )
-    class Meta():
+
+    class Meta:
         db_table = 'vacancy'
         verbose_name = 'Вакансия'
         verbose_name_plural = 'Вакансии'
 
+
 class Applicants(models.Model):
+    contact = models.ForeignKey(
+        Vacancy,
+        verbose_name="Соискатели",
+        on_delete=models.CASCADE,
+    )
     full_name = models.CharField(
         verbose_name="ФИО",
         max_length=200,
     )
-    phone =  models.CharField(
+    phone = models.CharField(
         verbose_name="Номер соискателя",
         max_length=13,
     )
@@ -95,7 +104,11 @@ class Applicants(models.Model):
         verbose_name='Дата отправки',
         auto_now_add=True,
     )
-    class Meta():
+    file = models.FileField(
+
+    )
+
+    class Meta:
         db_table = 'applicants'
         verbose_name = 'Соискатель'
         verbose_name_plural = 'Соискатели'
