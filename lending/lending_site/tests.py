7ca40d6253e0ab0d3918306c8e371523c4562558
paigni <ikontai4@gmail.com>
2022-11-29 20:19:56 +0600

    тесты в разработке

--- lending/lending_site/tests.py
+++ lending/lending_site/tests.py
@@ -3,11 +3,24 @@ from lending_site.service import (
     get_queryset_company,
     get_queryset_vacancy,
     )
+from lending_site.models import (
+    Company,
+    Contacts,
+    Vacancy,
+    Applicants
+)
 
 
-class ServiceTestCase():
+class ServiceTestCase(TestCase):
+    company  = Company()
+    contact = Contacts()
+    company.name = 'xxx'
+    company.description = 'xxxx'
+    contact.adress = 'xxxxx'
+    contact.phone = '314141'
+    contact.email = '12345@gmail.com' 
     def test_gqc(self):
         print(get_queryset_company)
 
     def test_gqv(self):
-        print(get_queryset_vacancy.name)
+        print(get_queryset_vacancy)
