6c77b08e9de4ab804c1f253dfc963de5e42125bb
paigni <ikontai4@gmail.com>
2022-11-29 20:16:50 +0600

    Сделаны новые функции для views

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import (
+    HttpResponse,
     render,
-    redirect,
-    get_object_or_404
     )
 from django.contrib import messages
 from django.views.generic import (
     ListView,
     )
-
-from lending_site.forms import ApplicantsForm
 from lending_site.service import (
     get_queryset_company,
     get_queryset_vacancy,
     )
+from lending_site.models import Applicants
+
 
 
 class IndexListView(ListView):
-    form = ApplicantsForm
-    template_name = 'lending_site/index.html'
-    if request.method == 'GET':
-        a = get_queryset_company
-        b = get_queryset_vacancy
+    def get(self, request ):
+        template_name = 'lending/index.html'
+        company_data = get_queryset_company
+        vacancy_data = get_queryset_vacancy
+        return render(request, template_name,vacancy_data,company_data)
+    
+    def post(self, request):
+        applicants = Applicants
+        full_name = applicants.full_name
+        phone = applicants.phone 
+        email = applicants.email
+        comment = applicants.comment
+        file = applicants.file
+        return HttpResponse('Ваше резюме отправлено,с вами свяжутся в ближайшее время')
+
 
