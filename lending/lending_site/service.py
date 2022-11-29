332ffbbda52a129475c0b69ac695d03e0cdd5ff7
paigni <ikontai4@gmail.com>
2022-11-29 20:19:34 +0600

    Изменнения в функциях

--- lending/lending_site/service.py
+++ lending/lending_site/service.py
@@ -2,12 +2,25 @@ from lending_site.models import Company,Vacancy
 
 
 def get_queryset_company():
-    company = Company
-    company_queryset = company.objects.all()
-    return company_queryset
-
+    company_queryset = Company.objects.filter(pk=1)
+    name = company_queryset.name
+    description = company_queryset.description
+    image = company_queryset.image
+    data = {
+        name:'name',
+        description:'description',
+        image:'image'
+    }
+    return data
 
 def get_queryset_vacancy():
-    vacancy = Vacancy
-    vacancy_queryset = vacancy.objects.filter(activated=1)
-    return vacancy_queryset
+    vacancy_queryset = Vacancy.objects.filter(activated=1)
+    name = vacancy_queryset.name
+    description = vacancy_queryset.description
+    salary = vacancy_queryset.salary
+    data = {
+        name:'name',
+        salary:'salary',
+        description:'description',
+        }
+    return data
