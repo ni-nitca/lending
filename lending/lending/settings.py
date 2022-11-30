9540fd29bed18c39bf4370840cef5aa70ae2c8b3
paigni <ikontai4@gmail.com>
2022-11-30 20:15:07 +0600

    правки

--- lending/lending/settings.py
+++ lending/lending/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lending_site.apps.LendingSiteConfig',
-    'files'
+    'lending_site',
 ]
 
 MIDDLEWARE = [
