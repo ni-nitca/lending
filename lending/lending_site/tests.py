192d7425d8ea6d57820b7736ba28f64af7fd9171
paigni <ikontai4@gmail.com>
2022-12-01 20:18:15 +0600

    Добавлены тесты

--- lending/lending_site/tests.py
+++ lending/lending_site/tests.py
@@ -13,19 +13,66 @@ from lending_site.models import (
 
 class ServiceTestCase(TestCase):
     @classmethod
+    @classmethod
     def setUpTestData(cls):
-        company  = Company()
-        contact = Contacts()
-        cls.c_name = 'xxx'
-        cls.description = 'xxxx'
-        cls.adress = 'xxxxx'
-        cls.phone = '314141'
-        cls.email = '12345@gmail.com'
-        cls.phone = '12345'
+        cls.c_name = 'Test'
+        cls.c_description = 'Testing'
+        cls.c_image = 'default.png'
+        cls.v_name = 'Test2'
+        cls.v_description = 'Testing2'
+        cls.salary = '300-400'
+        cls.activated = 'True'
+        cls.pk = 1
 
-    def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/index/')
-        self.assertEqual(resp.status_code, 200)
+
+        Company.objects.create(
+          name=cls.c_name,
+          description=cls.c_description,
+          image=cls.c_image
+        )
+
+        Vacancy.objects.create(
+            id = cls.pk,
+            v_name = cls.v_name,
+            description = cls.v_description,
+            salary = cls.salary,
+            activated = cls.activated
+        )
+
+
+    def test_cquery_is_true(self):
+        valid_dict = {
+          "name": self.c_name,
+          "description": self.c_description,
+          "image": self.c_image
+        }
+        invalid_dict = {
+            "description": self.c_description,
+            "image": 'pnx',
+            "name": self.c_name,
+        }
+        company_queryset = get_queryset_company()
+        self.assertEqual(company_queryset, valid_dict, msg=company_queryset)
+        self.assertNotEqual(company_queryset, invalid_dict, msg=company_queryset)
+
+    def test_vquery_is_true(self):
+        valid_dict = {
+        'name':self.v_name,
+        'id':self.pk,
+        'salary':self.salary,
+        'description':self.v_description,
+        }
+        invalid_dict = {
+        'name':self.v_name,
+        'id':2,
+        'salary':self.salary,
+        'description':self.v_description,
+        }
+        vacancy_queryset = get_queryset_vacancy()
+        print(vacancy_queryset)
+        self.assertEqual(vacancy_queryset, valid_dict, msg=vacancy_queryset)
+        self.assertNotEqual(vacancy_queryset, invalid_dict, msg=vacancy_queryset)
+    
 
 
     
