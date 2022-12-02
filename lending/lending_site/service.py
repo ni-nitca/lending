0979e4688b89cf461dbe8050daebfe8a6c6b0903
paigni <ikontai4@gmail.com>
2022-12-02 20:21:31 +0600

    Измененения во всех функциях,добавлена функция сохранения

--- lending/lending_site/service.py
+++ lending/lending_site/service.py
@@ -1,37 +1,53 @@
 from lending_site.models import (
     Company,
     Vacancy,
+    Applicants
     )
 
 
 def get_queryset_company():
-    company_queryset = Company.objects.get(pk=1)
-    name = company_queryset.name
-    description = company_queryset.description
-    image = company_queryset.image
+    company_queryset = Company.objects.get_or_create(pk=1)
     data = {
-        'name':name,
-        'description':description,
-        'image':image,
-    }
-    return data
-
-def get_queryset_vacancy():
-    vacancy_queryset = Vacancy.objects.get(activated=1)
-    v_name = vacancy_queryset.v_name
-    pk = vacancy_queryset.id
-    description = vacancy_queryset.description
-    salary = vacancy_queryset.salary
-    data = {
-        'name':v_name,
-        'id':pk,
-        'salary':salary,
-        'description':description,
+            'name':company_queryset[0].name,
+            'description': company_queryset[0].description,
+            'image': company_queryset[0].image
         }
     return data
 
-def get_context():
+def get_queryset_vacancy():
+    vacancy_queryset = Vacancy.objects.filter(activated=True).values()
+    data = list(vacancy_queryset)
+    if not data:
+        vacancy_queryset = Vacancy.objects.get_or_create()
+        vacancy_queryset = Vacancy.objects.filter(activated=True).values()
+        data = list(vacancy_queryset)
+        return data
+    else:
+        return data
+
+
+def save_applicants(
+    full_name,
+    phone,
+    email,
+    comment,
+    file
+    ):
+    applicants = Applicants
+    applicants.full_name = full_name 
+    applicants.phone = phone  
+    applicants.email = email 
+    applicants.comment = comment
+    applicants.file = file
+    applicants.save()
+
+
+def get_context(answer):
     company_data = get_queryset_company()
     vacancy_data = get_queryset_vacancy()
-    data = [company_data,vacancy_data]
+    data = {
+        "company":company_data,
+        "vacancy":vacancy_data,
+        "answer":answer
+    }
     return data
