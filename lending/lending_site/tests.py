c0a2dae47a78a0dcc3b6254cccf3253839ea3f28
paigni <ikontai4@gmail.com>
2022-12-02 20:21:41 +0600

    Обновлены тесты

--- lending/lending_site/tests.py
+++ lending/lending_site/tests.py
@@ -13,31 +13,16 @@ from lending_site.models import (
 
 class ServiceTestCase(TestCase):
     @classmethod
-    @classmethod
     def setUpTestData(cls):
-        cls.c_name = 'Test'
-        cls.c_description = 'Testing'
+        cls.c_name = 'text'
+        cls.c_description = 'text'
         cls.c_image = 'default.png'
         cls.v_name = 'Test2'
         cls.v_description = 'Testing2'
-        cls.salary = '300-400'
-        cls.activated = 'True'
+        cls.salary = '300'
+        cls.activated = True
         cls.pk = 1
 
-
-        Company.objects.create(
-          name=cls.c_name,
-          description=cls.c_description,
-          image=cls.c_image
-        )
-
-        Vacancy.objects.create(
-            id = cls.pk,
-            v_name = cls.v_name,
-            description = cls.v_description,
-            salary = cls.salary,
-            activated = cls.activated
-        )
 
 
     def test_cquery_is_true(self):
@@ -52,21 +37,21 @@ class ServiceTestCase(TestCase):
             "name": self.c_name,
         }
         company_queryset = get_queryset_company()
-        self.assertEqual(company_queryset, valid_dict, msg=company_queryset)
+        self.assertEqual(valid_dict, company_queryset, msg=company_queryset)
         self.assertNotEqual(company_queryset, invalid_dict, msg=company_queryset)
 
     def test_vquery_is_true(self):
         valid_dict = {
-        'name':self.v_name,
-        'id':self.pk,
-        'salary':self.salary,
-        'description':self.v_description,
+            'id':self.pk,
+            'name':self.v_name,
+            'salary':self.salary,
+            'description':self.v_description,
         }
         invalid_dict = {
-        'name':self.v_name,
-        'id':2,
-        'salary':self.salary,
-        'description':self.v_description,
+            'name':self.v_name,
+            'id':2,
+            'salary':self.salary,
+            'description':self.v_description,
         }
         vacancy_queryset = get_queryset_vacancy()
         print(vacancy_queryset)
