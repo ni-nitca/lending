affc5e25906fb836bc2f69534671175a2c7482a9
paigni <ikontai4@gmail.com>
2022-12-03 20:22:25 +0600

    Правки

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -17,15 +17,16 @@ from lending_site.models import Applicants
 class IndexListView(ListView):
     def get(self, request ):
         template_name = 'lending/index.html'
-        alplicants_answer = 0
+        alplicants_answer = False
         context = get_context(alplicants_answer)
         return render(request, template_name, context)
 
     def post(self, request):
-        applicants_answer = 1
+        applicants_answer = True
         template_name = 'lending/index.html'
         context = get_context(applicants_answer) 
-    
+        save_applicants(file_of_json)
+
         return render(request,template_name,context)
 
 
