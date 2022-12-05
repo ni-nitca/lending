a0878800cf7115b5f0d652d2d3a51e85faa216b4
paigni <ikontai4@gmail.com>
2022-12-05 20:51:08 +0600

    Исправлена view

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -16,17 +16,16 @@ from lending_site.models import Applicants
 
 class IndexListView(ListView):
     def get(self, request ):
-        template_name = 'lending/index.html'
+        template_name = 'lending_site/index.html'
         alplicants_answer = False
         context = get_context(alplicants_answer)
         return render(request, template_name, context)
 
     def post(self, request):
         applicants_answer = True
-        template_name = 'lending/index.html'
-        context = get_context(applicants_answer) 
-        save_applicants(file_of_json)
-
+        template_name = 'lending_site/index.html'
+        saved = save_applicants(file_of_json)
+        context = get_context(applicants_answer,saved) 
         return render(request,template_name,context)
 
 
