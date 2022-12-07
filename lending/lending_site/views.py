dbfe6947105176bbfcfbcfa7c2233dedd14f4bf7
paigni <ikontai4@gmail.com>
2022-12-07 19:21:59 +0600

    изменения во views

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -15,17 +15,17 @@ from lending_site.models import Applicants
 
 
 class IndexListView(ListView):
-    def get(self, request ):
+    def get(self, request):
         template_name = 'lending_site/index.html'
-        alplicants_answer = False
-        context = get_context(alplicants_answer)
+        context = get_context()
         return render(request, template_name, context)
 
     def post(self, request):
-        applicants_answer = True
         template_name = 'lending_site/index.html'
-        saved = save_applicants(file_of_json)
-        context = get_context(applicants_answer,saved) 
+        data = request.data
+        saved = save_applicants(data)
+        save_dict = {"saved":saved}
+        context = get_context() + save_dict
         return render(request,template_name,context)
 
 
