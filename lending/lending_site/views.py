266c6e832713f4d4a5d36efb3718d31d65a66fbf
paigni <ikontai4@gmail.com>
2022-12-09 19:39:23 +0600

    Правки

--- lending/lending_site/views.py
+++ lending/lending_site/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import (
-    HttpResponse,
     render,
-    )
+)
 from django.views.generic import (
     ListView,
-    )
+)
 from lending_site.service import (
     get_context,
     save_applicants
-
-    )
-from lending_site.models import Applicants
-
+)
 
 
 class IndexListView(ListView):
@@ -22,11 +18,9 @@ class IndexListView(ListView):
 
     def post(self, request):
         template_name = 'lending_site/index.html'
-        data = request.data
+        data = request.POST
+
         saved = save_applicants(data)
-        save_dict = {"saved":saved}
-        context = get_context() | save_dict
-        return render(request,template_name,context)
-    
-
-
+        context = get_context()
+        context['saved'] = saved
+        return render(request, template_name, context)
