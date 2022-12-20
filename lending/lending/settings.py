7369ff4e5a06d724ae528285d495261fd4f0211e
paigni <ikontai4@gmail.com>
2022-12-20 14:08:50 +0600

    обновлены настройки

--- lending/lending/settings.py
+++ lending/lending/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = "/var/www/lneding/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
